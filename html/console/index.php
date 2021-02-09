eace6ec4548b67f49091538b6a1686757c805ced
donotturnoff <admin@donotturnoff.net>
2021-02-09 16:41:38 +0000

    Add console shortcut cards

--- html/console/index.php
+++ html/console/index.php
@@ -7,8 +7,11 @@ make_header("Management console", "The Zoom Into Books management console", "");
   <main>
 <?php if (logged_in()) { ?>
 
-   <a href="books/">View your books</a>
-   <a href="resources/">View your resources</a>
+   <div class="card-container">
+    <a class="card" href="books/"><img src="/assets/images/icons/book-stack-128.png" alt=""/><span class="card-label">Manage your books</span></a>
+    <a class="card" href="books/new/"><img src="/assets/images/icons/plus-5-128.png" alt="" /><span class="card-label">Add a new book</span></a>
+    <a class="card" href="resources/"><img src="/assets/images/icons/pages-1-128.png" alt="" /><span class="card-label">View your resources</span></a>
+   </div>
 
 <?php } else {
     echo "   <p>You must <a href=\"/login/\">log in</a> to view this page.</p>\n";
