4e2917306b49fc2630d7065121504aaf186ded5b
donotturnoff <admin@donotturnoff.net>
2021-02-09 15:25:42 +0000

    Style login form

--- html/login/index.php
+++ html/login/index.php
@@ -9,16 +9,14 @@ make_header("Login", "", "");
 <?php
 if (!isset($_SESSION["username"])) {
 ?>
-   <form action="action.php" method="POST">
-    <label for="username-entry">Username</label>
-    <input type="text" name="username" id="username-entry" placeholder="Username" required="required" />
-    <label for="password-entry">Password</label>
-    <input type="password" name="password" id="password-entry" placeholder="Password" required="required" />
+   <form action="action.php" method="POST" id="login-form">
+    <input class="login-field" type="text" name="username" id="username-entry" placeholder="Username" required="required" />
+    <input class="login-field" type="password" name="password" id="password-entry" placeholder="Password" required="required" />
     <input type="submit" value="Login" />
    </form>
 <?php
 } else {
-    echo "   <p>You are already logged in as " . $_SESSION["username"] . ". You must <a href=\"/logout/\">log out before you can log in again.</p>\r\n";
+    echo "   <p>You are already logged in as " . $_SESSION["username"] . ". You must <a href=\"/logout/\">log out before you can log in again.</p>\n";
 }
 ?>
   </main>
