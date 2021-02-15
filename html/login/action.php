63750dc8c077d4dc613c6701ee60659e8ae9a8da
donotturnoff <admin@donotturnoff.net>
2021-02-15 10:16:54 +0000

    Temporarily change authentication setup

--- html/login/action.php
+++ html/login/action.php
@@ -4,7 +4,16 @@ require $_SERVER["DOCUMENT_ROOT"] . "/assets/modules/includes.php";
 if ($is_logged_in) {
     add_error("You are already logged in");
 } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    add_error("Authentication not implemented");
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $result = authorise($username, $password);
+    if ($result === "") {
+        add_error("Authentication failed");
+    } else {
+        $_SESSION["username"] = $username;
+        $_SESSION["account_type"] = $result;
+        set_success("Logged in as " . $username);
+    }
 } else {
     add_error("Login request method must be POST");
 }
