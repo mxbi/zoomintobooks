add9a8d4c0054465703a44ff07510e1caa0d900e
donotturnoff <admin@donotturnoff.net>
2021-02-15 10:16:22 +0000

    Add book list page

--- html/console/books/index.php
+++ html/console/books/index.php
@@ -9,8 +9,26 @@ $_SESSION["redirect"] = "/console/books/";
 <?php
 display_status();
 if ($is_logged_in) { ?>
+   <a class="book-list-item" id="book-list-add-book-item" href="new/">
+    <img src="/assets/images/icons/plus-5-128.png" alt="" />
+    <p>Add new book</p>
+   </a>
+<?php
+    $books = fetch_books(); // Load all books
+    foreach ($books as $book) {
+        $isbn = $book["isbn"];
+        $title = $book["title"];
+        $author = $book["author"];
+        $resource_count = count_resources($isbn);
+        echo "   <a class=\"book-list-item\" href=\"book?isbn=$isbn\">\n";
+        echo "    <img src=\"covers/$isbn.png\" alt=\"Front cover of $title\" />\n";
+        echo "    <p>$title</p>\n";
+        echo "    <p>$author</p>\n";
+        echo "    <p>$resource_count resources</p>\n";
+        echo "   </a>\n";
+    }
 
-<?php } else {
+} else {
     echo "   <p>You must <a href=\"/login/\">log in</a> to view this page.</p>\n";
 } ?>
   </main>
