8874037b532eb0461c9370aa04a17471904a23a8
donotturnoff <admin@donotturnoff.net>
2021-02-15 10:06:48 +0000

    Add more utility functions
    Validation, authentication, authorisation and error handling

--- html/assets/modules/utils.php
+++ html/assets/modules/utils.php
@@ -14,6 +14,53 @@ function init() {
     $is_admin = ($_SESSION["account_type"] === "admin");
 }
 
+function is_blank($s) {
+    return preg_match("/^\s*$/", $s) === 1;
+}
+
+function is_valid_isbn($isbn) {
+    // TODO
+    return true;
+}
+
+function is_pos_int($s) {
+    // TODO
+    return true;
+}
+
+function sanitise($s) {
+    global $dbc;
+    return mysqli_real_escape_string($dbc, htmlentities($s));
+}
+
+function authorise($username, $password) {
+    if ($username === "test") {
+        return "standard";
+    } else if ($username === "admin") {
+        return "admin";
+    } else {
+        return "";
+    }
+}
+
+function get_publisher() {
+    global $dbc;
+    $username = sanitise($_SESSION["username"]);
+    $q = "SELECT pub_id FROM user WHERE username = '$username'";
+    $r = mysqli_query($q);
+    if (!$r || mysqli_num_rows($dbc) != 1) {
+        add_error("Failed to get publisher for $username (" . mysqli_error($dbc) . ")");
+        return -1;
+    }
+    $pub_id = mysqli_fetch_array($r, MYSQLI_ASSOC)["pub_id"];
+    mysqli_free_result($r);
+    return $pub_id;
+}
+
+function errors_occurred() {
+    return !empty($_SESSION["errors"]);
+}
+
 function add_error($msg) {
     $_SESSION["errors"][] = $msg;
 }
