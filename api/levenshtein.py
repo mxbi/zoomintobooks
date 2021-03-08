da06e4b500c3843fba49dc3831edd045a28710b3
Victoria A <52001888+adjeiv@users.noreply.github.com>
2021-03-08 17:39:38 +0000

    Update levenshtein.py

--- api/levenshtein.py
+++ api/levenshtein.py
@@ -1,52 +1,12 @@
-def levenshtein_distance(str1, str2):
-	m = len(str1)
-	n = len(str2)
-	tmp1 = list(range(0, n+1))
-	tmp2 = [0]*(n+1)
-
-	for i in range(m):
-		tmp2[0] += 1
-		for j in range(n):
-			deletion_cost = tmp1[j+1] + 1
-			insertion_cost = tmp2[j] + 1
-			if str1[i] == str2[j]:
-				substitution_cost = tmp1[j]
-			else:
-				substitution_cost = tmp1[j] + 1
-
-			tmp2[j+1] = min(deletion_cost, insertion_cost, substitution_cost)
-
-		tmp = tmp2.copy()
-		tmp2 = tmp1.copy()
-		tmp1 = tmp
-
-
-	return tmp1[n]
-
-
-def prefix_match(str1, str2):
-	m = len(str1)
-	n = len(str2)
-	min_len = min(m, n)
-	pref = 0
-
-	for i in range(min_len):
-		if str1[i] != str2[i]:
-			break
-		pref += 1
-
-	return pref
-
+import difflib
 
 def match(match_str, word_list):
-	for word in word_list:
-		word[2] = prefix_match(match_str, word[0].lower())
-
-	word_list.sort(key=lambda x: x[2], reverse=True)
-
-	for word in word_list[:2]:
-		word[2] = levenshtein_distance(match_str, word[0].lower())
-
-	word_list[:2].sort(key=lambda x: x[2])
-
-	return word_list[:2]
+	result = []
+	titles = [t[0] for t in word_list]
+	closest_matches = difflib.get_close_matches(match_str, titles, cutoff=0.1)
+	for res in closest_matches:
+		i = titles.index(res)
+		if i != -1:
+			result.append(word_list[i][1])
+			
+	return result
