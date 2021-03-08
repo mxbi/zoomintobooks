7b7a65597ad4bb8bedf88453f02579a69a6de161
donotturnoff <admin@donotturnoff.net>
2021-03-08 10:52:32 +0000

    Fix position of floating preview bubble

--- html/console/resources/resource/index.php
+++ html/console/resources/resource/index.php
@@ -20,7 +20,7 @@ $authorised = authorised("edit resource", array("rid" => $rid));
 display_status();
 if ($authorised) {
     echo "    <h3>Edit resource properties</h3>\n";
-    echo "<div class=\"preview-container\"><a href=\"$url\">View<img class=\"preview\" id=\"resource-preview-img\" src=\"preview?rid=$rid\" alt=\"\" /></a></div>\n";
+    echo "<div class=\"preview-container\"><a href=\"$url\"><span>View</span><img class=\"preview\" id=\"resource-preview-img\" src=\"preview?rid=$rid\" alt=\"\" /></a></div>\n";
     show_resource_form(true, $rid);
     // TODO: show books using this resource
     if (authorised("delete resource", array("rid" => $rid), false)) {
