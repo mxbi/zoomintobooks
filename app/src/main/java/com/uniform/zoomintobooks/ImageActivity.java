a8b6ac6b50bcfd0e9e5ef7fc2424b145c3003b80
Peixuan Song <60033220+31958@users.noreply.github.com>
2021-03-08 11:38:05 +0000

    Export image resource to Download

--- app/src/main/java/com/uniform/zoomintobooks/ImageActivity.java
+++ app/src/main/java/com/uniform/zoomintobooks/ImageActivity.java
@@ -1,72 +1,52 @@
 package com.uniform.zoomintobooks;
 
-import android.Manifest;
-import android.content.Intent;
+import android.Manifest;;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
-import android.os.Build;
-import android.provider.MediaStore;
+import android.os.Environment;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 
 import android.widget.Toast;
-import androidx.annotation.NonNull;
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-import com.uniform.zoomintobooks.R;
-
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+
 
 public class ImageActivity extends AppCompatActivity implements View.OnClickListener {
-
-    private static final int GALLERY_REQUEST_CODE = 100;
-    private static final int CAMERA_REQUEST_CODE = 200;
-    private static final int STORAGE_PERMISSION_CODE = 1234;
-
     private ScaleGestureDetector mScaleGestureDetector;
     private float mScaleFactor = 1.0f;
 
-    private EditText editText;
-    private Button sendButton;
     private ImageView imageView;
-    private Button dstBtn;
-    private Button srcBtn;
-    private Button cameraBtn;
+    private Button exportBtn;
 
-    private Uri filePath;
+    private Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view);
 
-        requestStoragePermission();
-        requestCameraPermission();
+        imageView = findViewById(R.id.scalableImage);
 
-        imageView = (ImageView) findViewById(R.id.postImage);
-        Uri uri = (Uri) getIntent().getExtras().get("uri");
+        uri = (Uri) getIntent().getExtras().get("uri");
         imageView.setImageURI(uri);
 
         mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
 
-        dstBtn = (Button) findViewById(R.id.pickImageButton);
-        cameraBtn = (Button) findViewById(R.id.cameraBtn);
+        exportBtn = findViewById(R.id.exportButton);
 
-        dstBtn.setOnClickListener(this);
-        cameraBtn.setOnClickListener(this);
+        exportBtn.setOnClickListener(this);
     }
 
     @Override
@@ -76,7 +56,6 @@ public class ImageActivity extends AppCompatActivity implements View.OnClickList
 
     private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
 
-        // when a scale gesture is detected, use it to resize the image
         @Override
         public boolean onScale(ScaleGestureDetector scaleGestureDetector){
             mScaleFactor *= scaleGestureDetector.getScaleFactor();
@@ -87,111 +66,65 @@ public class ImageActivity extends AppCompatActivity implements View.OnClickList
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        //For gallery
-        if (resultCode == RESULT_OK && requestCode == GALLERY_REQUEST_CODE) {
-            try {
-                Uri selectedImage = data.getData();
-                InputStream imageStream = getContentResolver().openInputStream(selectedImage);
-                imageView.setImageBitmap(BitmapFactory.decodeStream(imageStream));
-            } catch (IOException exception) {
-                exception.printStackTrace();
-            }
-        }
-
-        //For camera
-        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-        }
-
-        //For upload
-        if (requestCode == GALLERY_REQUEST_CODE && resultCode == RESULT_OK && data.getData() != null) {
-            filePath = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
-                imageView.setImageBitmap(bitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void requestStoragePermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
-    }
-
-    private void requestCameraPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == STORAGE_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission granted", Toast.LENGTH_LONG).show();
-            }
-            else {
-                Toast.makeText(this, "Permission not granted", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-
-    public void openGallery(View view) {
-        Intent intent = new Intent();
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select a picture"), GALLERY_REQUEST_CODE);
-    }
-
-    public void openCamera(View view) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
-        }
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private String getPath (Uri uri) {
-        Cursor cursor = getContentResolver().query(uri, null, null, null);
-        cursor.moveToFirst();
-        String document_id = cursor.getString(0);
-
-        document_id = document_id.substring(document_id.lastIndexOf(":") + 1);
-        cursor.close();
-
-        cursor = getContentResolver().query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                null,
-                MediaStore.Images.Media._ID + " = ? ",
-                new String[]{document_id},
-                null
-        );
-        cursor.moveToFirst();
-        String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
-        cursor.close();
-        return path;
-    }
-
-    @Override
     public void onClick(View v) {
-        if (v == dstBtn) {
-            openGallery(v);
+        if (v == exportBtn) {
+            saveImage();
+        }
+    }
+
+    private boolean requestPermissions() {
+        return PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    }
+
+    private String GALLERY_DIRECTORY = Environment.getExternalStorageDirectory() + "/" + Environment.DIRECTORY_DOWNLOADS;
+
+
+    public void saveImage(){
+        boolean permissionGranted = requestPermissions();
+        String uri = this.uri.toString();
+        String filename = uri.substring(uri.lastIndexOf("/"));
+        export(uri, GALLERY_DIRECTORY.concat(filename));
+
+        Toast.makeText(this, "File saved to ".concat(GALLERY_DIRECTORY.concat(filename)), Toast.LENGTH_LONG).show();
+    }
+
+    private void export(String src, String dst){
+        String state = Environment.getExternalStorageState();
+        if (!Environment.MEDIA_MOUNTED.equals(state)) {
+            return;
         }
 
-        if (v == cameraBtn) {
-            openCamera(v);
+        try {
+            FileInputStream inputStream = null;
+            FileOutputStream outputStream = null;
+
+            File fileSrc = new File(src);
+            File fileDst = new File(dst);
+
+            inputStream = new FileInputStream(fileSrc);
+            outputStream = new FileOutputStream(fileDst, true);
+            fileDst.createNewFile();
+
+            byte[] buf = new byte[8192];
+            int length = 0;
+            while (true) {
+                try {
+                    if (!((length = inputStream.read(buf)) > 0)) break;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                outputStream.write(buf, 0, length);
+            }
+
+            outputStream.close();
+            inputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
 
     }
 }
