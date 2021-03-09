9c480ff3ef56165f1e30a0f2811fb7658549f74d
donotturnoff <admin@donotturnoff.net>
2021-03-09 12:10:01 +0000

    Add credits

--- html/assets/modules/footer.php
+++ html/assets/modules/footer.php
@@ -1,5 +1,8 @@
 <?php
 function make_footer() {?>
+  <footer>
+   <small><a href="/credits.php">Credits</a></small>
+  </footer>
  </body>
 </html>
 <?php
--- html/credits.php
+++ html/credits.php
@@ -10,8 +10,10 @@ make_header("Credits", "", "");
     <li>Open Sans is licensed under <a href="/assets/fonts/open_sans/LICENCE.txt">Apache 2.0</a></li>
    </ul>
    <h3>Icons</h3>
+   <p>All icons apart from our logo are from <a href="https://www.iconsdb.com/">IconsDB</a>. Click any of the links to see the original.</p>
    <ul>
-    
+    <li>The <a href="https://www.iconsdb.com/white-icons/plus-5-icon.html">plus</a>, <a href="https://www.iconsdb.com/white-icons/briefcase-6-icon.html">briefcase</a> and <a href="https://www.iconsdb.com/white-icons/headphones-4-icon.html">headphones</a> icons are licensed under <a href="https://creativecommons.org/publicdomain/zero/1.0/legalcode">CC0 1.0 Universal (CC0 1.0) Public Domain Dedication</a></li>
+    <li>The <a href="https://www.iconsdb.com/white-icons/group-icon.html">group</a>, <a href="https://www.iconsdb.com/white-icons/book-stack-icon.html">book stack</a>, <a href="https://www.iconsdb.com/white-icons/geography-icon.html">globe</a> and <a href="https://www.iconsdb.com/white-icons/pages-1-icon.html">pages</a> icons are licensed under <a href="https://creativecommons.org/licenses/by-nd/3.0/legalcode">Creative Commons Attribution-NoDerivs 3.0</a></li>
    </ul>
   </main>
 
