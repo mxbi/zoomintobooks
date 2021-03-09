dfee6981c6526b12ddaa46adb153a6e0df089833
donotturnoff <admin@donotturnoff.net>
2021-03-09 11:50:58 +0000

    Add Ana's changes to README

---
+++ html/credits.php
@@ -0,0 +1,20 @@
+<?php 
+require "assets/modules/includes.php";
+make_header("Credits", "", "");
+?>
+
+  <h2>Credits</h2>
+  <main>
+   <h3>Fonts</h3>
+   <ul>
+    <li>Open Sans is licensed under <a href="/assets/fonts/open_sans/LICENCE.txt">Apache 2.0</a></li>
+   </ul>
+   <h3>Icons</h3>
+   <ul>
+    
+   </ul>
+  </main>
+
+<?php
+make_footer();
+?>
