5ca76889b14504368d9f8e06491ce0a29f7bc076
ww348 <ww348@cam.ac.uk>
2021-03-09 01:21:55 +0100

    Remove Book info activity

--- app/src/main/java/com/uniform/zoomintobooks/BarcodeScanActivity.java
+++ app/src/main/java/com/uniform/zoomintobooks/BarcodeScanActivity.java
@@ -148,12 +148,7 @@ public class BarcodeScanActivity extends AppCompatActivity {
             startActivity(startIntent);
         });
 
-        FloatingActionButton BookButton = findViewById(R.id.BookButton);
-        BookButton.setOnClickListener(view -> {
-            Intent startIntent = new Intent(getApplicationContext(), BookActivity.class);
-            startIntent.putExtra("CurrentAct",BarcodeScanActivity.class);
-            startActivity(startIntent);
-        });
+
 
         FloatingActionButton ContactButton = findViewById(R.id.ContactButton);
         ContactButton.setOnClickListener(view -> {
@@ -184,19 +179,16 @@ public class BarcodeScanActivity extends AppCompatActivity {
         FloatingActionButton MoreButton = findViewById(R.id.MoreButton);
         FloatingActionButton InfoButton = findViewById(R.id.InfoButton);
         FloatingActionButton ContactButton = findViewById(R.id.ContactButton);
-        FloatingActionButton BookButton = findViewById(R.id.BookButton);
         FloatingActionButton SettingButton = findViewById(R.id.SettingButton);
         if(!moreButtonOpen){
             MoreButton.setAnimation(rotateOpen);
             InfoButton.setAnimation(fromBottom);
             ContactButton.setAnimation(fromBottom);
-            BookButton.setAnimation(fromBottom);
             SettingButton.setAnimation(fromBottom);
         } else {
             MoreButton.setAnimation(rotateClose);
             InfoButton.setAnimation(toBottom);
             ContactButton.setAnimation(toBottom);
-            BookButton.setAnimation(toBottom);
             SettingButton.setAnimation(toBottom);
         }
     }
@@ -204,17 +196,14 @@ public class BarcodeScanActivity extends AppCompatActivity {
     private void setVisibilityOfMenu(Boolean moreButtonOpen) {
         FloatingActionButton InfoButton = findViewById(R.id.InfoButton);
         FloatingActionButton ContactButton = findViewById(R.id.ContactButton);
-        FloatingActionButton BookButton = findViewById(R.id.BookButton);
         FloatingActionButton SettingButton = findViewById(R.id.SettingButton);
         if(!moreButtonOpen){
             InfoButton.setVisibility(View.VISIBLE);
             ContactButton.setVisibility(View.VISIBLE);
-            BookButton.setVisibility(View.VISIBLE);
             SettingButton.setVisibility(View.VISIBLE);
         } else {
             InfoButton.setVisibility(View.INVISIBLE);
             ContactButton.setVisibility(View.INVISIBLE);
-            BookButton.setVisibility(View.INVISIBLE);
             SettingButton.setVisibility(View.INVISIBLE);
         }
     }
--- app/src/main/java/com/uniform/zoomintobooks/WelcomeActivity.java
+++ app/src/main/java/com/uniform/zoomintobooks/WelcomeActivity.java
@@ -56,12 +56,6 @@ public class WelcomeActivity extends AppCompatActivity {
             startActivity(startIntent);
         });
 
-        FloatingActionButton BookButton = findViewById(R.id.BookButton);
-        BookButton.setOnClickListener(view -> {
-            Intent startIntent = new Intent(getApplicationContext(), BookActivity.class);
-            startIntent.putExtra("CurrentAct",WelcomeActivity.class);
-            startActivity(startIntent);
-        });
 
         FloatingActionButton ContactButton = findViewById(R.id.ContactButton);
         ContactButton.setOnClickListener(view -> {
@@ -92,19 +86,16 @@ public class WelcomeActivity extends AppCompatActivity {
         FloatingActionButton MoreButton = findViewById(R.id.MoreButton);
         FloatingActionButton InfoButton = findViewById(R.id.InfoButton);
         FloatingActionButton ContactButton = findViewById(R.id.ContactButton);
-        FloatingActionButton BookButton = findViewById(R.id.BookButton);
         FloatingActionButton SettingButton = findViewById(R.id.SettingButton);
         if (!moreButtonOpen) {
             MoreButton.setAnimation(rotateOpen);
             InfoButton.setAnimation(fromBottom);
             ContactButton.setAnimation(fromBottom);
-            BookButton.setAnimation(fromBottom);
             SettingButton.setAnimation(fromBottom);
         } else {
             MoreButton.setAnimation(rotateClose);
             InfoButton.setAnimation(toBottom);
             ContactButton.setAnimation(toBottom);
-            BookButton.setAnimation(toBottom);
             SettingButton.setAnimation(toBottom);
         }
     }
@@ -112,17 +103,14 @@ public class WelcomeActivity extends AppCompatActivity {
     private void setVisibilityOfMenu(Boolean moreButtonOpen) {
         FloatingActionButton InfoButton = findViewById(R.id.InfoButton);
         FloatingActionButton ContactButton = findViewById(R.id.ContactButton);
-        FloatingActionButton BookButton = findViewById(R.id.BookButton);
         FloatingActionButton SettingButton = findViewById(R.id.SettingButton);
         if (!moreButtonOpen) {
             InfoButton.setVisibility(View.VISIBLE);
             ContactButton.setVisibility(View.VISIBLE);
-            BookButton.setVisibility(View.VISIBLE);
             SettingButton.setVisibility(View.VISIBLE);
         } else {
             InfoButton.setVisibility(View.INVISIBLE);
             ContactButton.setVisibility(View.INVISIBLE);
-            BookButton.setVisibility(View.INVISIBLE);
             SettingButton.setVisibility(View.INVISIBLE);
         }
     }
